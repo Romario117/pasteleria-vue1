96ce01cbcc949043158fec5b32a5ef484ed88c7d
Romario117 <rfls_93@hotmail.com>
2022-03-24 12:59:11 -0600

    Agregacion de Nuevo Menu responsivo

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,42 @@
 <template>
-  <nav class="primary">
-    <div class="row">
-      <div class="col-11">
-        <ul class="fs3-normal center">
-          <li><img src="assets/logo.png" width="85px" height="75px" alt=""></li>
-          <li><router-link to="/inicio">Inicio</router-link></li>
-          <li><router-link to="/pasteles">Pasteles</router-link></li>
-          <li><router-link to="/galletas">Galletas</router-link></li>
-          <li>
-            <router-link to="/especialidades"> Especialidades</router-link>
-          </li>
+  <nav class="navbar navbar-expand-lg navbar-light bg-nav">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" to="/">
+    <img src="assets/LogoPasteleria.png">
+    Pastelerias PanCake</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/inicio">Inicio</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/pasteles">Pasteles</router-link>
+        </li>
+        <li class="nav-item">
+         <router-link class="nav-link" to="/galletas">Galletas</router-link>
+        </li>
+                <li class="nav-item">
+         <router-link class="nav-link" to="/especialidades">Especialidades</router-link>
+        </li>
+                       
+      </ul>
+       <ul class="d-flex">
+         <router-link class="nav-link" to="/login">Iniciar Sesion</router-link>
         </ul>
-      </div>
-      <div class="col-1 center button-primary">
-        <ul>
-          <li><router-link to="/login">Iniciar Sesion</router-link></li>
-        </ul>
-      </div>
     </div>
-  </nav>
+  </div>
+</nav>
 
 
-<div class="footer primary">
-    Patelerias PanCake.  Tula de Allende,Hidalgo. CP. 42800. Tel: 777-77-777-77
-</div>
+
+
   <router-view />
 </template>
+
+
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
@@ -37,7 +48,7 @@
   text-align: center;
   color: #2c3e50;
 }
-.primary {
+.bg-nav {
   background-color: #fd7e14;
   color: black;
 }
@@ -79,7 +90,7 @@ li a {
 
 /* Change the link color to #111 (black) on hover */
 li a:hover {
-  background-color: #111;
+  background-color: rgb(250, 246, 246);
 }
 
 div {
