048ebc865c963f02aa34c2bff4cb684a7f193086
Romario117 <rfls_93@hotmail.com>
2022-03-24 13:00:09 -0600

    Dashboard de informacion y promociones

---
+++ src/components/Dashboard1.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <div class="row">
+          <h3>Ubicanos en</h3>
+        </div>
+        <div class="row">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1874.0495729506704!2d-99.33570869941899!3d20.046289497039204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x716997175dbe5564!2zMjDCsDAyJzQ2LjYiTiA5OcKwMjAnMDYuMiJX!5e0!3m2!1ses-419!2smx!4v1648145871617!5m2!1ses-419!2smx"
+            width="400"
+            height="300"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="row">
+          <h5>Direccion :</h5>
+            <h6>Tula de Allende, Hgo. </h6>
+        </div>
+        <div class="row">
+          <h5>Telefono :</h5>
+          <h6>77-777-777-77</h6>
+        </div>
+        <div class="row">
+          <h5>Correo Electronico :</h5>
+          <h6>pasteleriasPancake@mail.com</h6>
+        </div>
+        <div class="row">
+            <img src="../assets/LogoPasteleria.png">
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="row">
+          <h3>Promociones del mes :</h3>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="card-group">
+              <div class="row">
+                <div class="card" style="width: 18rem">
+                  <img
+                    src="../assets/logo.png"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Pastel de Queso</h5>
+                    <p class="card-text">Rico pastel de queso.</p>
+                    <a href="#" class="btn btn-orange">Ver pastel</a>
+                  </div>
+                </div>
+                <div class="card" style="width: 18rem">
+                  <img
+                    src="../assets/logo.png"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Pastel de Queso</h5>
+                    <p class="card-text">Rico pastel de queso.</p>
+                    <a href="#" class="btn btn-orange">Ver pastel</a>
+                  </div>
+                </div>
+                <div class="card" style="width: 18rem">
+                  <img
+                    src="../assets/logo.png"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Pastel de Queso</h5>
+                    <p class="card-text">Rico pastel de queso.</p>
+                    <a href="#" class="btn btn-orange">Ver pastel</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.btn-orange {
+  background-color: #fd7e14;
+  color: black;
+}
+</style>
