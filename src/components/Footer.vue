68fefc793e369e66b1d50bd33f9b40707d3e8e12
Romario117 <rfls_93@hotmail.com>
2022-03-24 13:00:19 -0600

    Footer v.1

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+  <footer class="">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+  </div>
+</template>
