e29e8029e7c656f559f63b1591a5c1446dde4bcc
Romario117 <rfls_93@hotmail.com>
2022-03-24 12:59:28 -0600

    Agregacion de JS de Boostrap

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
+import "bootstrap/dist/js/bootstrap.min.js"
+
 createApp(App).use(store).use(router).mount('#app')
